4330e82ee10da95d6d4f4c1465aeaba0c8030f20
judeharding <jharding@genesisgroupusa.com>
2018-10-17 16:14:30 -0400

    starting counter app

---
+++ react-app/src/index.js
@@ -0,0 +1,9 @@
+// importing objects from react modules
+import React from "react";
+import ReactDOM from "react-dom";
+
+// defining an element.  const is new feature in es6 like var or let
+const element = <h1>Hello World</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById("root"));
